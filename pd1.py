7bae2b757371c313126d4486efd990fef5b8f646
ggoitia999-spec <ggoitia999@gmail.com>
2026-04-27 18:46:25 -0300

    main

---
+++ pd1.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+df = pd.read_csv('ClassicDisco.csv')
+#print(df.to_string()) #imprime toda os dados da planilha
+#print(df.head) #exibe as 5 primeiras linhas
+#print(df.tail) #exibe as 5 últimas linhas
+
+print(df.shape)
---
+++ pd2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+dados = {
+    'cargos': ["assistente", "analista", "gerente", "diretor"], 
+    'salarios': [1000, 2000, 3000, 4000]
+}
+
+dados_bi = pd.DataFrame(dados)
+print(dados_bi.tail(3))
+dados_bi.to_csv('meus dados.csv', index=False, sep=';', encoding='utf-8')
+
+print("Arquivo exportado com sucesso!")
